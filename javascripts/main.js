c142857cb2f5b2a9eeb4526e127c945cf6bf9c23
William Jackson <williampj@gmail.com>
2020-12-23 19:15:28 -0600

    styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {});
+function handleCaseStudyNav() {
+  const caseStudyNav = document.getElementById('case-study-nav');
+  const caseStudyNavVisible = () => 700 <= window.pageYOffset;
+
+  document.addEventListener('scroll', () => {
+    if (caseStudyNavVisible()) {
+      caseStudyNav.style.visibility = 'visible';
+      return;
+    }
+
+    caseStudyNav.style.visibility = 'hidden';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleCaseStudyNav();
+});
