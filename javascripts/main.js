9029d75dab031d6ea1b36c9555acd39e8f178263
Andrew <awcrotwell@gmail.com>
2020-12-26 17:44:51 -0500

    Eslint + Clean up js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,41 @@
 function handleCaseStudyNav() {
   const caseStudyNav = document.getElementById('case-study-nav');
-  const caseStudyNavVisible = () => 700 <= window.pageYOffset;
+  const caseStudyNavVisible = () => window.pageYOffset >= 700;
 
-  document.addEventListener('scroll', () => {
+  /**
+   * Handles Showing/Hiding Navbar
+   */
+  const handleNavVisiblity = () => {
     if (caseStudyNavVisible()) {
       caseStudyNav.style.visibility = 'visible';
       return;
     }
 
     caseStudyNav.style.visibility = 'hidden';
+  };
+
+  const caseStudyTitles = document.querySelectorAll('.case-study-h1');
+  let activeLi;
+  const handleNavActive = async () => {
+    caseStudyTitles.forEach((title, idx) => {
+      if (
+        window.pageYOffset >= title.offsetTop
+        && window.pageYOffset <= caseStudyTitles[idx + 1].offsetTop
+      ) {
+        if (activeLi) activeLi.classList.remove('active');
+
+        activeLi = caseStudyNav
+          .children[0] // ul
+          .children[idx]; // li
+
+        activeLi.classList.add('active');
+      }
+    });
+  };
+
+  document.addEventListener('scroll', () => {
+    handleNavVisiblity();
+    handleNavActive();
   });
 }
 
