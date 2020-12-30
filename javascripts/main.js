f3eae01c18a6302a48f563d6f00e089d25117fc2
Andrew <awcrotwell@gmail.com>
2020-12-30 13:11:49 -0500

    Main menu, lighthouse image, eb text

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,8 @@
+/**
+ * Handles fading out the sidebar navigation
+ *
+ * @param {HTMLElement} el
+ */
 function fadeOut(el) {
   el.style.opacity = 1;
   (function fade() {
@@ -9,6 +14,12 @@ function fadeOut(el) {
   }());
 }
 
+/**
+ * Handles fading in the sidebar navigation
+ *
+ * @param {HTMLElement} el
+ * @param {string} display
+ */
 function fadeIn(el, display) {
   el.style.opacity = 0;
   el.style.display = display || 'block';
@@ -69,6 +80,45 @@ function handleCaseStudyNav() {
   });
 }
 
+const handleMainNavigation = () => {
+  const navigation = document.getElementById('site-navigation');
+
+  document.getElementById('burger-menu')
+    .addEventListener('click', () => {
+      if (navigation.style.display === 'none') {
+        navigation.style.display = 'block';
+      } else {
+        navigation.style.display = 'none';
+      }
+    });
+
+  /**
+   * Handles highlighting the nav's current section based
+   * on current screen location.
+   */
+  const mobileNav = document.getElementById('case-study-mobile');
+  const caseStudyTitles = document.querySelectorAll('.case-study-h1');
+  let activeLi;
+  const handleNavActive = async () => {
+    caseStudyTitles.forEach((title, idx) => {
+      if (window.pageYOffset >= title.offsetTop - 10) {
+        if (activeLi) activeLi.classList.remove('active');
+
+        activeLi = mobileNav
+          .children[0] // ul
+          .children[idx]; // li
+
+        activeLi.classList.add('active');
+      }
+    });
+  };
+
+  document.addEventListener('scroll', () => {
+    handleNavActive();
+  });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   handleCaseStudyNav();
+  handleMainNavigation();
 });
